589964831ddc326006950c355c1e0d30e09e0240
Diego Najar <dignajar@gmail.com>
2021-06-28 11:01:34 +0200

    include comments for methods

--- files/security.py
+++ files/security.py
@@ -5,6 +5,9 @@ class Security:
         return None
 
     def getUserIP(self):
+        '''
+            Returns the user's IP
+        '''
         if request.environ.get('HTTP_X_REAL_IP') is not None:
             return request.environ.get('HTTP_X_REAL_IP')
         elif request.environ.get('HTTP_X_FORWARDED_FOR') is not None:
@@ -13,4 +16,7 @@ class Security:
             return request.remote_addr
 
     def getUserReferrer(self):
+        '''
+            Returns the user's referrer
+        '''
         return request.base_url
